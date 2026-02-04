9b5d8d84045fe5aadec48dd35797b25286eae899
Saif Abu Hananah <saif9@pdx.edu>
2026-02-04 01:52:02 -0800

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,8 +1,8 @@
 // File: tree.cpp
 // Author: Saif Abu Hananah
 // Date: 22/May/2025
-// Program 3: Monstertale - A Monster-Taming Game
-// Class: CS302
+// Monstertale - A Monster-Taming Game
+
 #include "tree.h"
 #include <iostream>
 
